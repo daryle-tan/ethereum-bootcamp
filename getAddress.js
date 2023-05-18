fd597a07b7bd7dcd59240523fec685c6066dd539
daryle <daryle.tan@gmail.com>
2023-05-17 23:28:15 -0500

    changed first variable name

--- getAddress.js
+++ getAddress.js
@@ -2,8 +2,8 @@ const secp = require("ethereum-cryptography/secp256k1")
 const { keccak256 } = require("ethereum-cryptography/keccak")
 
 function getAddress(publicKey) {
-  const firstByte = publicKey.slice(1)
-  const keccakHash = keccak256(firstByte)
+  const noFirstByte = publicKey.slice(1)
+  const keccakHash = keccak256(noFirstByte)
   return keccakHash.slice(-20)
 }
 
