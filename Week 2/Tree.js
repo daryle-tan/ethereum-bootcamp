e3cfbe36d77d76ac0d41eb14fb2259f4ea5ad8a2
daryle <daryle.tan@gmail.com>
2023-06-08 23:03:48 -0500

    creating a Merkle Tree

--- Week 2/Tree.js
+++ Week 2/Tree.js
@@ -9,25 +9,46 @@ class Tree {
       this.root = node
       //
     } else if (node.data < parent.data) {
-      if (!parent.left) {
-        parent.left = node
-      } else {
-        let newTree = new Tree()
-        newTree.addNode(parent.left)
-        newTree.addNode(node)
-        parent.left = newTree.root
-      }
+        if (!parent.left) {
+          parent.left = node
+        } else {
+          let newTree = new Tree()
+          newTree.addNode(parent.left)
+          newTree.addNode(node)
+          parent.left = newTree.root
+        }
+      
     } else {
-      if (!parent.right) {
-        parent.right = node
-      } else {
-        let newTree = new Tree()
-        newTree.addNode(parent.right)
-        newTree.addNode(node)
-        parent.right = newTree.root
-      }
+        if (!parent.right) {
+          parent.right = node
+        } else {         
+            let newTree = new Tree()
+            newTree.addNode(parent.right)
+            newTree.addNode(node)
+            parent.right = newTree.root
+        }
     }
   }
+
+  hasNode(num){
+    const searchNode = (node, num) => {
+      if(!node) {
+        return false;
+      }
+      if (node.data === num) {
+        return true;
+      } 
+      if (node.data > num) {
+        return searchNode(node.left, num);
+      }
+      if(node.data < num){
+        return searchNode(node.right, num);
+      }
+      return false;
+    }
+    return searchNode(this.root, num);
+  }
+
 }
 
 module.exports = Tree
