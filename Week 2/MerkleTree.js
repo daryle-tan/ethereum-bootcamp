8efda5ca7386573063475de719809b80b7c9cda5
daryle <daryle.tan@gmail.com>
2023-06-09 23:22:35 -0500

    find the root of of a merkle tree even with an odd number of nodes

--- Week 2/MerkleTree.js
+++ Week 2/MerkleTree.js
@@ -3,8 +3,24 @@ class MerkleTree {
     this.leaves = leaves
     this.concat = concat
   }
-  getRoot() {
-    return this.concat(this.leaves[0], this.leaves[1])
+  getRoot(leaves = this.leaves) {
+    let layer = []
+    // if there is only 1 leaf node left, then that is the root node
+    if (leaves.length === 1) {
+      console.log("here", leaves)
+      return leaves[0]
+    }
+    // loop through the tree and combine the leaves
+    for (let i = 0; i < leaves.length; i += 2) {
+      // add the pair of leaves to create a layer
+      if (leaves[i + 1]) {
+        layer.push(this.concat(leaves[i], leaves[i + 1]))
+      } else {
+        layer.push(leaves[i])
+      }
+    }
+    // recursively call the function until there is 1 node left
+    return this.getRoot(layer)
   }
 }
 
