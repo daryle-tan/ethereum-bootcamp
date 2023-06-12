92c49d3e1c1742db90158daaa85b68ab805c421d
daryle <daryle.tan@gmail.com>
2023-06-11 19:34:14 -0500

    insert and contain method for trie

--- Week 2/Trie.js
+++ Week 2/Trie.js
@@ -2,7 +2,38 @@ const TrieNode = require("./TrieNode")
 
 class Trie {
   constructor() {
+    // root will be an instance of TrieNode that contains a null key
     this.root = new TrieNode(null)
+  }
+
+  insert(word) {
+    let node = this.root
+
+    for (let i = 0; i < word.length; i++) {
+      // if the current letter is not a child of the node
+      if (!node.children[word[i]]) {
+        // create a child node for the root node.
+        node.children[word[i]] = new TrieNode(word[i])
+      }
+      node = node.children[word[i]]
+
+      if (i === word.length - 1) {
+        node.isWord = true
+      }
+    }
+    console.log(node)
+  }
+
+  contains(word) {
+    let node = this.root
+    for (let i = 0; i < word.length; i++) {
+      if (node.children[word[i]]) {
+        node = node.children[word[i]]
+      } else {
+        return false
+      }
+    }
+    return node.isWord
   }
 }
 
