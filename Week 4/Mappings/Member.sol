a6ae0e1cd1ecc5b41149ca5f1e74f8a9d2f3c447
daryle <daryle.tan@gmail.com>
2023-07-02 21:37:32 -0500

    more mapping

--- Week 4/Mappings/Member.sol
+++ Week 4/Mappings/Member.sol
@@ -5,15 +5,15 @@ contract Member {
     // in mapping, the key has to be a value data type but the value can be any type such as the reference data type
     mapping(address => bool) public members;
 
-    function addMember(address memb) external {
+    function addMember(address _memb) external {
         members[memb] = true;
     }
 
-    function isMember(address memb) external view returns (bool) {
+    function isMember(address _memb) external view returns (bool) {
         return members[memb];
     }
 
-    function removeMember(address memb) external {
+    function removeMember(address _memb) external {
         members[memb] = false;
     }
 }
