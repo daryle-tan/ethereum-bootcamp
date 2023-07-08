ce19c5b6f896e4836daad37d66635e8386644783
daryle <daryle.tan@gmail.com>
2023-07-08 14:03:20 -0500

    structs lesson 5-6

---
+++ Week 5/structs/ChangeVote.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract Contract {
+    enum Choices {
+        Yes,
+        No
+    }
+
+    struct Vote {
+        Choices choice;
+        address voter;
+    }
+
+    // mapping(address => bool) private hasCalled;
+    // custom error
+    error CanOnlyBeCalledOnce();
+    // TODO: create a public state variable: an array of votes
+    Vote[] public votes;
+
+    Vote none = Vote(Choices(0), address(0));
+
+    function createVote(Choices choice) public {
+        if (hasVoted(msg.sender)) {
+            revert CanOnlyBeCalledOnce();
+        }
+        // hasCalled[msg.sender] = true;
+        // TODO: add a new vote to the array of votes state variable
+        votes.push(Vote(choice, msg.sender));
+    }
+
+    function changeVote(Choices choice) external {
+        Vote storage vote = findVote(msg.sender);
+        require(vote.voter != none.voter, "No exisiting vote.");
+        vote.choice = choice;
+    }
+
+    function findVote(address voter) internal view returns (Vote storage) {
+        for (uint i = 0; i < votes.length; i++) {
+            if (voter == votes[i].voter) {
+                return votes[i];
+            }
+        }
+        return none;
+    }
+
+    function hasVoted(address _voter) public view returns (bool) {
+        return findVote(_voter).voter == _voter;
+    }
+
+    function findChoice(address _voter) external view returns (Choices) {
+        return findVote(_voter).choice;
+    }
+}
