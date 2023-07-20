dabf809fb8265be01c95d8347a42382f8a08e531
daryle <daryle.tan@gmail.com>
2023-07-20 00:47:38 -0500

    6.1.4

--- Week 6/MultiSig.sol
+++ Week 6/MultiSig.sol
@@ -3,13 +3,31 @@ pragma solidity ^0.8.4;
 
 contract MultiSig {
     address[] public owners;
-    uint256 public required;
+    uint public transactionCount;
+    uint public required;
 
-    constructor(address[] memory _owner, uint256 _required) {
-        require(_owner.length > 0, "Need owner addresses");
-        require(_required > 0, "Need more confirmations");
-        require(_required <= _owner.length, "Too many confirmations");
-        owners = _owner;
-        required = _required;
+    struct Transaction {
+        address destination;
+        uint value;
+        bool executed;
+    }
+
+    mapping(uint => Transaction) public transactions;
+
+    constructor(address[] memory _owners, uint _confirmations) {
+        require(_owners.length > 0);
+        require(_confirmations > 0);
+        require(_confirmations <= _owners.length);
+        owners = _owners;
+        required = _confirmations;
+    }
+
+    function addTransaction(address destination, uint value)
+        public
+        returns (uint transactionId)
+    {
+        transactionId = transactionCount;
+        transactions[transactionCount] = Transaction(destination, value, false);
+        transactionCount += 1;
     }
 }
