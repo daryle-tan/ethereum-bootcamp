701b43f1cac42fc0cab7bacd1f9f799df02b0dce
daryle <daryle.tan@gmail.com>
2023-07-22 13:19:20 -0500

    6.2.4 super.modify()

--- Week 6/SuperHeroes.sol
+++ Week 6/SuperHeroes.sol
@@ -4,8 +4,20 @@ pragma solidity 0.8.4;
 import "./Hero.sol";
 
 // TODO: create Mage/Warrior Heroes
-contract Mage is Hero {
+contract Mage is Hero(50) {
+    function attack(address enemy) public override {
+        Enemy _enemy = Enemy(enemy);
+        _enemy.takeAttack(Hero.AttackTypes.Spell);
 
+        Hero.attack(enemy);
+    }
 }
 
-contract Warrior is Hero {}
+contract Warrior is Hero(200) {
+    function attack(address enemy) public override {
+        Enemy _enemy = Enemy(enemy);
+        _enemy.takeAttack(Hero.AttackTypes.Brawl);
+
+        super.attack(enemy);
+    }
+}
