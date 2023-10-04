7ff732a2e3d557f0579a1ee90361c319d42fd1a1
daryle <daryle.tan@gmail.com>
2023-10-03 21:13:20 -0700

    bytes32 and string literals

--- SolidityCourse/contract.sol
+++ SolidityCourse/contract.sol
@@ -5,4 +5,8 @@ contract Contract {
     int8 public a = 1;
     int8 public b = -1;
     int16 public difference = a - b;
+
+    bytes32 public msg1 = "Hello World";
+    string public msg2 =
+        "Hello you morons, Please be clearer with your instructions. Kthnx";
 }
