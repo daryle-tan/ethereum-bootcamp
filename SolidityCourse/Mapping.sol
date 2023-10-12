46c1ae5daa58b2fb52bb8b2c850eb3c3eded61b9
daryle <daryle.tan@gmail.com>
2023-10-11 21:52:46 -0700

    mapping fin

--- SolidityCourse/Mapping.sol
+++ SolidityCourse/Mapping.sol
@@ -7,4 +7,57 @@ contract Contract {
     function addMember(address _member) external {
         members[_member] = true;
     }
+
+    function isMember(address _member) external returns (bool) {
+        return members[_member];
+    }
+
+    function removeMember(address _member) external {
+        members[_member] = false;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    struct User {
+        uint balance;
+        bool isActive;
+    }
+
+    mapping(address => User) public users;
+
+    function createUser() external {
+        require(!users[msg.sender].isActive);
+        users[msg.sender] = User(100, true);
+    }
+
+    function transfer(address recipient, uint amount) external {
+        require(users[msg.sender].isActive && users[recipient].isActive);
+        require(users[msg.sender].balance >= amount);
+        users[msg.sender].balance -= amount;
+        users[recipient].balance += amount;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    enum ConnectionTypes {
+        Unacquainted,
+        Friend,
+        Family
+    }
+
+    // TODO: create a public nested mapping `connections`
+    mapping(address => mapping(address => ConnectionTypes)) public connections;
+
+    function connectWith(address other, ConnectionTypes connectionType)
+        external
+    {
+        // TODO: make the connection from msg.sender => other => connectionType
+        connections[msg.sender][other] = connectionType;
+    }
 }
